123b80bd49f3202a22f442ce627a52c9a20aaf11
woopackerpat <p.taechakraichana@gmail.com>
2024-01-19 11:37:51 +0700

    CommonJS

---
+++ data.js
@@ -0,0 +1,7 @@
+const a = [1, 2, 3, 4];
+
+const testFunc = () => {
+  console.log("Test");
+};
+
+module.exports = { a, testFunc };
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log("Woody")
+console.log("Woodyttttgggg")
