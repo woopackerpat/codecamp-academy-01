a4830a80b0a060fbc738e0eab20d089d1ebf5385
woopackerpat <p.taechakraichana@gmail.com>
2024-01-19 11:56:46 +0700

    node-buildin-libraly

--- data.js
+++ data.js
@@ -1,7 +1,12 @@
-const a = [1, 2, 3, 4];
+// const a = [1, 2, 3, 4];
 
-const testFunc = () => {
-  console.log("Test");
-};
+// const testFunc = () => {
+//   console.log("Test");
+// };
 
-module.exports = { a, testFunc };
+// module.exports = { a, testFunc };
+// module.exports = testFunc;
+
+// exports.testFunc = () => {
+//   console.log("Test");
+// };
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Woodyttttgggg")
+// const obj = require("./data")
+
+// console.log(obj)
+
+// const { a, testFunc } = require("./data");
+
+// console.log(a);
+// console.log(testFunc);
+
+// const helloFunc = require("./data");
+
+// console.log(helloFunc());
+
+// const { testFunc } = require("./data");
+// testFunc();
+
+// console.log(__dirname)
+// console.log(__filename)
+
+const path = require("path");
+
+console.log(__dirname + "/" + "data.js");
+console.log(path.join(__dirname, "data.js"));
