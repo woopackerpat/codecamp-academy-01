3116553bc890dc6420037e5a57842112dfb52511
woopackerpat <p.taechakraichana@gmail.com>
2024-01-19 15:49:55 +0700

    express lab1

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Hello");
-  res.json({message: "Hello"})
+// app.use((req, res, next) => {
+//   console.log("Hello");
+//   res.json({message: "Hello"})
+// });
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "GET" });
+// });
+// app.post("/", (req, res) => {});
+// app.put("/", (req, res) => {});
+// app.patch("/", (req, res) => {});
+// app.delete("/", (req, res) => {});
+
+app.get("/", (req, res) => {
+  res.json({ title: "My First Web App", message: "Our Web App API" });
 });
 
 app.listen("8000", () => {
