a6f6a14e5c99983338aeb74cedf36c723a5b2b30
woopackerpat <p.taechakraichana@gmail.com>
2024-01-19 10:28:38 +0700

    npm init

---
+++ basic/index.js
@@ -0,0 +1,39 @@
+// const a = [1, 2, 3, 4, 5];
+
+// console.log("test1");
+// const b = a.map((el) => el * 2);
+
+// console.log(b);
+
+// console.log("test2");
+
+// const testPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Hello world");
+//   }, 2000);
+// });
+
+// console.log("test1");
+// testPromise.then((res) => console.log(res));
+// console.log("test2");
+
+// const doAnything = (doSth) => {
+//   const num1 = 5;
+//   const num2 = 10;
+
+//   const result = doSth(num1, num2);
+
+//   return result;
+// };
+
+// const multiply = (numA, numB) => {
+//   return numA * numB;
+// };
+
+// const newResult = doAnything(multiply);
+
+// console.log(newResult);
+
+// global.setTimeout(() => {
+//   console.log("test");
+// }, 2000);
