641148971c23fb074628c93258fb380f97fd9ae0
woopackerpat <p.taechakraichana@gmail.com>
2024-01-19 09:38:37 +0700

    callback function

--- index.js
+++ index.js
@@ -7,12 +7,29 @@
 
 // console.log("test2");
 
-const testPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Hello world");
-  }, 2000);
-});
+// const testPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Hello world");
+//   }, 2000);
+// });
 
-console.log("test1");
-testPromise.then((res) => console.log(res));
-console.log("test2");
+// console.log("test1");
+// testPromise.then((res) => console.log(res));
+// console.log("test2");
+
+const doAnything = (doSth) => {
+  const num1 = 5;
+  const num2 = 10;
+
+  const result = doSth(num1, num2);
+
+  return result;
+};
+
+const multiply = (numA, numB) => {
+  return numA * numB;
+};
+
+const newResult = doAnything(multiply);
+
+console.log(newResult);
