5822a591ddb2ae5850c104fb9116d86ecb2d585e
woopackerpat <p.taechakraichana@gmail.com>
2024-01-19 15:19:10 +0700

    app-use

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("Hello");
+  res.json({message: "Hello"})
+});
+
 app.listen("8000", () => {
   console.log("Server is running on port 8000");
 });
