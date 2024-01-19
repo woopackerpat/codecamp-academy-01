4f3f7de3e5a062f0da4dda672d55a11221355c3e
woopackerpat <p.taechakraichana@gmail.com>
2024-01-19 13:33:10 +0700

    fs/promise

--- index.js
+++ index.js
@@ -22,13 +22,29 @@
 // console.log(__dirname + "/" + "data.js");
 // console.log(path.join(__dirname, "data.js"));
 
-const fs = require("fs");
+// const fs = require("fs");
+// const path = require("path");
+
+// fs.readFile(
+//   path.join(__dirname, "hello.txt"),
+//   { encoding: "utf-8" },
+//   (err, data) => {
+//     // console.log(data);
+//     fs.writeFile("hello2.txt",data,() => {
+//         console.log("Done writeFile")
+//     })
+//   }
+// );
+
+const fs = require("fs/promises");
 const path = require("path");
 
-fs.readFile(
-  path.join(__dirname, "hello.txt"),
-  { encoding: "utf-8" },
-  (err, data) => {
-    console.log(data);
-  }
-);
+const run = async () => {
+  const data = await fs.readFile(path.join(__dirname, "hello.txt"), {
+    encoding: "utf-8",
+  });
+
+  console.log(data);
+};
+
+run();
