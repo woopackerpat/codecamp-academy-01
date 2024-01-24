c563d96e2be480bddf55d8ae3ea961df22c05aa4
woopackerpat <p.taechakraichana@gmail.com>
2024-01-24 15:16:14 +0700

    createPost

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,13 +1,61 @@
+const prisma = require("../config/prisma");
+const createError = require("../utils/createError");
 exports.getAllPosts = async (req, res, next) => {
   res.json({ message: "Get all posts" });
 };
 
 exports.getPostById = async (req, res, next) => {
-  res.json({ message: "Get Post By Id" });
+  try {
+    const { postId } = req.params;
+
+    if (isNaN(Number(postId))) {
+      return createError(400, "Post id should be number");
+    }
+
+    const post = await prisma.post.findFirst({
+      where: {
+        id: Number(postId),
+      },
+    });
+
+    if (!post) {
+      return createError(400, "Post not found");
+    }
+
+    res.json({ post });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.createPost = async (req, res, next) => {
-  res.json({ message: "Create post" });
+  try {
+    const { title, published, authorId } = req.body;
+
+    if (!title || !authorId) {
+      return createError(400, "Invalid data");
+    }
+
+    if (typeof published !== "boolean") {
+      return createError(400, "Publish data should boolean");
+    }
+
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        published,
+        author: {
+          connect: {
+            id: authorId,
+          },
+        },
+      },
+    });
+
+    res.json({ post: newPost });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.updatePost = async (req, res, next) => {
