4288199f7428a26fdaa34ad81ef3fbdbc48b8718
woopackerpat <p.taechakraichana@gmail.com>
2024-01-24 14:35:49 +0700

    post router

--- routes/post-route.js
+++ routes/post-route.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const router = express.Router();
+
+router.get("/", () => {});
+router.get("/:postId", () => {});
+router.post("/", () => {});
+router.update("/:postId", () => {});
+router.delete("/:postId", () => {});
+
+module.exports = router;
