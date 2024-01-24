de7d49dc92bba42d3b30bef5641fca9fadf11119
woopackerpat <p.taechakraichana@gmail.com>
2024-01-24 14:04:42 +0700

    findUnique-create-incldue

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,39 @@
-exports.register = (req, res, next) => {
-  res.status(201).json({ message: "Register success" });
+const prisma = require("../config/prisma");
+const createError = require("../utils/createError");
+
+exports.register = async (req, res, next) => {
+  try {
+    const { email, password, bio } = req.body;
+
+    const userExist = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userExist) {
+      return createError(400, "User already exist");
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password,
+        profile: {
+          create: {
+            bio,
+          },
+        },
+      },
+      include: {
+        profile: true,
+      },
+    });
+
+    res.status(201).json({ user: newUser });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.login = (req, res, next) => {
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const authRoute = require("./routes/auth-route");
+
+const errorHandler = require("./middlewares/error");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/auth", authRoute);
+
+app.use(errorHandler);
 
 app.listen(8000, () => {
   console.log("Server run on port 8000");
