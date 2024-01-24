298e02e28cf2098f06242081ebf7a293754672e1
woopackerpat <p.taechakraichana@gmail.com>
2024-01-24 15:55:55 +0700

    done post

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,7 +1,27 @@
 const prisma = require("../config/prisma");
 const createError = require("../utils/createError");
 exports.getAllPosts = async (req, res, next) => {
-  res.json({ message: "Get all posts" });
+  try {
+    const { search } = req.query;
+
+    const posts = await prisma.post.findMany({
+      where: {
+        title: search,
+      },
+      take: 3
+    //   orderBy: {
+    //     id: "desc"
+    //   }
+    });
+
+    if (posts.length <= 0) {
+      return createError(400, "Post not found");
+    }
+
+    res.json({ posts });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.getPostById = async (req, res, next) => {
@@ -59,9 +79,43 @@ exports.createPost = async (req, res, next) => {
 };
 
 exports.updatePost = async (req, res, next) => {
-  res.json({ message: "Update Post" });
+  const { postId } = req.params;
+  const { title, published } = req.body;
+
+  const post = await prisma.post.findFirst({
+    where: {
+      id: Number(postId),
+    },
+  });
+
+  if (!post) {
+    return createError(400, "Post not found");
+  }
+
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: post.id,
+    },
+    data: {
+      title,
+      published,
+    },
+  });
+
+  res.json({ post: updatedPost });
 };
 
 exports.deletPost = async (req, res, next) => {
-  res.json({ message: "Delete Post" });
+  const { postId } = req.params;
+  try {
+    await prisma.post.delete({
+      where: {
+        id: Number(postId),
+      },
+    });
+
+    res.status(204).json({ message: "Delete Post" });
+  } catch (err) {
+    next(err);
+  }
 };
